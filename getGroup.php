76f49f854850ca8d0c04a65ffbf276fb43d9f39d
Corné van Willigen <c.j.c.van.willigen@student.rug.nl>
2012-01-30 21:01:27 +0100

    added counters and total value

--- getGroup.php
+++ getGroup.php
@@ -18,6 +18,7 @@ echo "<table border='1'>
 <th>ID</th>
 <th>Fullname</th>
 <th>comment</th>
+<th>value</th>
 </tr>";
 
 while($row = mysql_fetch_array($result))
@@ -26,9 +27,25 @@ while($row = mysql_fetch_array($result))
   echo "<td>" . $row['ID_group'] . "</td>";
   echo "<td>" . $row['username'] . "</td>";
   echo "<td>" . $row['comment'] . "</td>";
+  echo "<td>" . $row['value'] . "</td>";
   echo "</tr>";
   }
 echo "</table>";
+
+
+$query = "SELECT SUM(value) ,COUNT(value) FROM payments WHERE ID_group = '".$q."'"; 
+	 
+$result_table = mysql_query($query) or die(mysql_error());
+
+// Print out result
+while($row = mysql_fetch_array($result_table)){
+	echo "Total amount for the group ". $row['value']. " = € ". $row['SUM(value)']. ",-";
+	echo "<br />";
+	$number_rows = mysql_num_rows($result_table);
+	$part = $row['SUM(value)'] / $row['COUNT(value)'];
+	echo "Your part = € " .round($part,2). ",-";
+	
+}
 
 mysql_close($con);
 ?>
